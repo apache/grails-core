fdfc89c2a7759ef97a404e2682461cb100087092
Mattias Reichel <mattias.reichel@gmail.com>
2025-01-22 11:57:29 +0100

    test: replace deprecated `@RunWith` with `@Suite`
    - Update tests to use the `@Suite` annotation, replacing the deprecated `@RunWith`.

--- grails-datastore-gorm-test/src/test/groovy/org/grails/datastore/gorm/SimpleMapTestSuite.groovy
+++ grails-datastore-gorm-test/src/test/groovy/org/grails/datastore/gorm/SimpleMapTestSuite.groovy
@@ -1,15 +1,13 @@
 package org.grails.datastore.gorm
 
 import grails.gorm.tests.NotInListSpec
-import org.junit.platform.runner.JUnitPlatform
 import org.junit.platform.suite.api.SelectClasses
-import org.junit.runner.RunWith
+import org.junit.platform.suite.api.Suite
 
 /**
  * @author graemerocher
  */
-//TODO: Replace with JUnit5 declarative test suites once https://github.com/junit-team/junit5/issues/744 is resolved
-@RunWith(JUnitPlatform)
+@Suite
 @SelectClasses([NotInListSpec])
 class SimpleMapTestSuite {
 }
