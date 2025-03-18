2161bf8e8236fefd80a38534d58ff04d22fc8dba
Mattias Reichel <mattias.reichel@gmail.com>
2025-03-18 13:46:11 +0100

    refactor: move classes out of `grails-app`
    Simplify the project by keeping all sources in `src/main/groovy`.

--- src/main/groovy/grails/plugins/mail/PlainTextMailTagLib.groovy
+++ src/main/groovy/grails/plugins/mail/PlainTextMailTagLib.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2005 the original author or authors.
+ * Copyright 2004-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,12 @@
  */
 package grails.plugins.mail
 
+import grails.artefact.Artefact
+
+@Artefact('TagLib')
 class PlainTextMailTagLib {
-    static namespace = "text"
+
+    static namespace = 'text'
 
     def newLine = {
         out << '\n'
