3b150777e54048d15d9b42eaff2013eb645d204d
huangyucai <280400880@qq.com>
2025-05-21 23:45:53 +0800

    Fix config retrieval to use get('grails.redis', Map) for compatibility with newer Grails versions 备注：RedisGrailsPlugin 获取 grails.redis 总是返回[:]，因为升级后的grails中getProperty只支持返回string。

--- plugin/src/main/groovy/redis/RedisGrailsPlugin.groovy
+++ plugin/src/main/groovy/redis/RedisGrailsPlugin.groovy
@@ -40,7 +40,7 @@ class RedisGrailsPlugin extends Plugin {
 
     Closure doWithSpring() {
         { ->
-            def redisConfigMap = grailsApplication.config.getProperty('grails.redis') ?: [:]
+            def redisConfigMap = grailsApplication.config.get('grails.redis') ?: [:]
 
             RedisConfigurationUtil.configureService(delegate, redisConfigMap, "", RedisService)
             redisConfigMap?.connections?.each { connection ->
