e0fadf0ae5efb101ca46b755ab61e4316f8baaad
Walter Duque de Estrada <wbduque@mac.com>
2026-01-29 10:49:08 -0600

    progress

---
+++ grails-data-hibernate6/core/src/main/groovy/org/grails/orm/hibernate/cfg/domainbinding/collectionType/BagCollectionType.java
@@ -0,0 +1,28 @@
+package org.grails.orm.hibernate.cfg.domainbinding.collectionType;
+
+import org.hibernate.MappingException;
+import org.hibernate.boot.spi.InFlightMetadataCollector;
+import org.hibernate.mapping.Bag;
+import org.hibernate.mapping.Collection;
+import org.hibernate.mapping.PersistentClass;
+
+import org.grails.datastore.mapping.model.types.ToMany;
+import org.grails.orm.hibernate.cfg.GrailsDomainBinder;
+
+public class BagCollectionType extends CollectionType {
+
+    public BagCollectionType(GrailsDomainBinder binder) {
+        super(java.util.Collection.class, binder);
+    }
+
+    @Override
+    public Collection create(ToMany property, PersistentClass owner,
+                             String path, InFlightMetadataCollector mappings, String sessionFactoryBeanName) throws MappingException {
+        Bag coll = new Bag(buildingContext, owner);
+        coll.setCollectionTable(owner.getTable());
+        coll.setTypeName(getTypeName(property));
+        binder.bindCollection(property, coll, owner, mappings, path, sessionFactoryBeanName);
+        return coll;
+    }
+
+}
