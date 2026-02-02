70cb6ac59b4f59d2ef32be8cf6ea71cb40c2238d
Walter Duque de Estrada <wbduque@mac.com>
2026-02-02 13:48:11 -0600

    Fixed tests

--- grails-data-hibernate7/core/src/main/groovy/org/grails/orm/hibernate/cfg/GrailsHibernatePersistentEntity.java
+++ grails-data-hibernate7/core/src/main/groovy/org/grails/orm/hibernate/cfg/GrailsHibernatePersistentEntity.java
@@ -1,10 +1,12 @@
 package org.grails.orm.hibernate.cfg;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.grails.datastore.mapping.model.PersistentEntity;
 import org.grails.datastore.mapping.model.PersistentProperty;
+import org.grails.datastore.mapping.model.config.GormProperties;
 import org.grails.orm.hibernate.cfg.domainbinding.ConfigureDerivedPropertiesConsumer;
 
 /**
@@ -33,10 +35,23 @@ public interface GrailsHibernatePersistentEntity extends PersistentEntity {
 
     boolean isAbstract();
 
-
-    @SuppressWarnings("unchecked")
-    default List<GrailsHibernatePersistentProperty> getHibernatePersistentProperties() {
-        return (List) getPersistentProperties();
+    /**
+     * @return The properties that should be bound to the Hibernate meta model
+     */
+    default List<GrailsHibernatePersistentProperty> getPersistentPropertiesToBind() {
+        List<PersistentProperty> properties = getPersistentProperties();
+        if (properties == null) {
+            return java.util.Collections.emptyList();
+        }
+        return properties.stream()
+                .filter(Objects::nonNull)
+                .filter(p -> p.getMappedForm() != null)
+                .filter(p -> !p.isIdentityProperty())
+                .filter(p -> !p.isCompositeIdProperty())
+                .filter(p -> !GormProperties.VERSION.equals(p.getName()))
+                .filter(p -> !p.isInherited())
+                .map(p -> (GrailsHibernatePersistentProperty) p)
+                .toList();
     }
 
     @Override
