ee718c97d894ae1019084d1cb408ad4328fcacc0
Walter Duque de Estrada <wbduque@mac.com>
2026-03-05 07:50:56 -0600

    cleanup(hibernate7): ConfigureDerivedPropertiesConsumer

--- grails-data-hibernate7/core/src/main/groovy/org/grails/orm/hibernate/cfg/domainbinding/util/ConfigureDerivedPropertiesConsumer.java
+++ grails-data-hibernate7/core/src/main/groovy/org/grails/orm/hibernate/cfg/domainbinding/util/ConfigureDerivedPropertiesConsumer.java
@@ -24,19 +24,17 @@ import java.util.function.Consumer;
 import org.grails.orm.hibernate.cfg.Mapping;
 import org.grails.orm.hibernate.cfg.domainbinding.hibernate.HibernatePersistentProperty;
 
-import static java.util.Optional.ofNullable;
+public class ConfigureDerivedPropertiesConsumer implements Consumer<HibernatePersistentProperty> {
 
 public class ConfigureDerivedPropertiesConsumer implements Consumer<HibernatePersistentProperty> {
 
     private final Mapping m;
 
-    public ConfigureDerivedPropertiesConsumer(Mapping m) {
-        this.m = m;
-    }
-
-    @Override
-    public void accept(HibernatePersistentProperty persistentProperty) {
-        ofNullable(m.getPropertyConfig(persistentProperty.getName()))
-                .ifPresent(propertyConfig -> propertyConfig.setDerived(Objects.nonNull(propertyConfig.getFormula())));
-    }
+  @Override
+  public void accept(HibernatePersistentProperty persistentProperty) {
+    ofNullable(m.getPropertyConfig(persistentProperty.getName()))
+        .ifPresent(
+            propertyConfig ->
+                propertyConfig.setDerived(Objects.nonNull(propertyConfig.getFormula())));
+  }
 }
