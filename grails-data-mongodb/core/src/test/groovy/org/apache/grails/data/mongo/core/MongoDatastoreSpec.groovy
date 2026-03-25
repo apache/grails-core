6eef1672919e28b413d82cc37c4688d33cf49a1d
Walter Duque de Estrada <wbduque@mac.com>
2026-03-24 21:20:10 -0500

    mongo: starts MongoDatastoreSpec

--- grails-data-mongodb/core/src/test/groovy/org/apache/grails/data/mongo/core/MongoDatastoreSpec.groovy
+++ grails-data-mongodb/core/src/test/groovy/org/apache/grails/data/mongo/core/MongoDatastoreSpec.groovy
@@ -1,35 +1,29 @@
 package org.apache.grails.data.mongo.core
 
-import org.testcontainers.spock.Testcontainers
-import spock.lang.Shared
-
 import org.apache.grails.data.testing.tck.base.GrailsDataTckSpec
 import org.apache.grails.testing.mongo.AbstractMongoGrailsExtension
 import org.grails.datastore.mapping.mongo.config.MongoSettings
 import org.testcontainers.containers.MongoDBContainer
 import spock.lang.Requires
 
-@Testcontainers
 @Requires({ isDockerAvailable() })
 abstract class MongoDatastoreSpec extends GrailsDataTckSpec<GrailsDataMongoTckManager> {
 
-    @Shared MongoDBContainer container = new MongoDBContainer(AbstractMongoGrailsExtension.desiredMongoDockerName)
+    static MongoDBContainer mongoDBContainer
 
     void setupSpec() {
-        if (container != null && !container.isRunning()) {
-            container.start()
+        if (isDockerAvailable()) {
+            if (mongoDBContainer == null) {
+                mongoDBContainer = new MongoDBContainer(AbstractMongoGrailsExtension.desiredMongoDockerName)
+                mongoDBContainer.start()
+            }
+            manager.mongoDBContainer = mongoDBContainer
+            manager.configuration = [
+                    (MongoSettings.SETTING_DATABASE_NAME): 'test',
+                    (MongoSettings.SETTING_HOST)         : mongoDBContainer.host,
+                    (MongoSettings.SETTING_PORT)         : mongoDBContainer.getMappedPort(AbstractMongoGrailsExtension.DEFAULT_MONGO_PORT) as String
+            ]
         }
-        manager.mongoDBContainer = container
-        manager.configuration = [
-                (MongoSettings.SETTING_DATABASE_NAME): 'test',
-                (MongoSettings.SETTING_HOST)         : container.host,
-                (MongoSettings.SETTING_PORT)         : container.getMappedPort(AbstractMongoGrailsExtension.DEFAULT_MONGO_PORT) as String
-        ]
-    }
-
-    void cleanupSpec() {
-        // We let Testcontainers handle shutdown via its Ryuk container, or we could stop it here 
-        // if we wanted to recreate it per-spec. Keeping it static across the suite is faster.
     }
 
     static boolean isDockerAvailable() {
