ae7fb5a2b66704b0afc4d6cddc9d07155b7a6d97
jdaugherty <jdaugherty@users.noreply.github.com>
2026-03-31 21:06:03 +0000

    [skip ci] Release v7.0.10

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -33,7 +33,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("7.0.10-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("7.0.10", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
