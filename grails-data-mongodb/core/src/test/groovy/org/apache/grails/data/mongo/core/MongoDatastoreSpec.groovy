f87096a0a1a4dd978c01d11344c15a008d1fe174
James Daugherty <jdaugherty@jdresources.net>
2026-04-11 09:03:49 -0400

    Remove remnants of old docker container

--- grails-data-mongodb/core/src/test/groovy/org/apache/grails/data/mongo/core/MongoDatastoreSpec.groovy
+++ grails-data-mongodb/core/src/test/groovy/org/apache/grails/data/mongo/core/MongoDatastoreSpec.groovy
@@ -19,12 +19,6 @@
 package org.apache.grails.data.mongo.core
 
 import org.apache.grails.data.testing.tck.base.GrailsDataTckSpec
-import spock.lang.Requires
 
-@Requires({ isDockerAvailable() })
 abstract class MongoDatastoreSpec extends GrailsDataTckSpec<GrailsDataMongoTckManager> {
-
-    static boolean isDockerAvailable() {
-        GrailsDataMongoTckManager.isDockerAvailable()
-    }
 }
