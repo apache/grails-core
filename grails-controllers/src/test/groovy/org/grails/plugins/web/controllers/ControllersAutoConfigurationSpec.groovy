ba081ef6c8f6889c84ff01bc140a0781b21421b6
g.sartori <g.sartori@gmail.com>
2026-07-22 09:59:39 +0200

    Revert "Merge fixes & cleanups"
    This reverts commit d1ca11fc663192167367efae9e14874684d03a70.

--- grails-controllers/src/test/groovy/org/grails/plugins/web/controllers/ControllersAutoConfigurationSpec.groovy
+++ grails-controllers/src/test/groovy/org/grails/plugins/web/controllers/ControllersAutoConfigurationSpec.groovy
@@ -19,12 +19,13 @@
 
 package org.grails.plugins.web.controllers
 
+import org.springframework.beans.factory.BeanCreationException
+import org.springframework.core.env.MapPropertySource
 import java.util.function.Supplier
 
 import grails.core.DefaultGrailsApplication
 import grails.core.GrailsApplication
 
-import org.springframework.beans.factory.BeanCreationException
 import org.springframework.boot.autoconfigure.AutoConfigurations
 import org.springframework.boot.test.context.runner.WebApplicationContextRunner
 import org.springframework.boot.web.servlet.AbstractFilterRegistrationBean
@@ -33,12 +34,11 @@ import org.springframework.boot.web.servlet.ServletContextInitializerBeans
 import org.springframework.boot.webmvc.autoconfigure.WebMvcAutoConfiguration
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.core.env.MapPropertySource
 import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.mock.web.MockHttpServletResponse
 import org.springframework.mock.web.MockServletContext
-import org.springframework.web.context.WebApplicationContext
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
+import org.springframework.web.context.WebApplicationContext
 import org.springframework.web.context.support.StaticWebApplicationContext
 import org.springframework.web.filter.RequestContextFilter
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver
